d6bd69f9b84e63b3fc0da799f1481795bedd684b
Chua Soon Dee <chuasoondee@soondee-mb.local>
2014-03-02 22:46:32 +0800

    Fixed jenkins volume not mapped to host dir

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+[ -d $DIR/jenkins/work ] || mkdir $DIR/jenkins/work
+
 $DOCKER run -i -t \
     --name="$container" \
     -h="$container.devcloud.com" \
     -p 8080:8080 -p 2222:22 \
-    -v $DIR/jenkins/home:/home/jenkins \
-    -rm=true \
+    -v $DIR/jenkins/work:/home/jenkins \
+    -d \
     "sdchua/$container"
